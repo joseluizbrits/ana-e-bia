b269907a552dad499997ae0b5a05069441f4939c
brits <joseluizbrits@gmail.com>
2023-10-16 11:48:45 -0300

    create past containers

--- src/containers/Products/Products.tsx
+++ src/containers/Products/Products.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Section } from "./ProdutosStyled";
+import { Section } from "./ProductsStyled";
 import Button from "../Button/Button";
 import { products } from "../../utils/products";
 
