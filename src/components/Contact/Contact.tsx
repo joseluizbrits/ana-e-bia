f03ff06213b6a7d4351eb199afc479b86985464b
brits <joseluizbrits@gmail.com>
2023-10-18 20:28:39 -0300

    useForm and Field component for forms

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import useForm from "../../hooks/useForm";
+import Cake from "../../assets/svg/Cake";
+import Field from "./Field";
+import emailjs from "@emailjs/browser";
+
+function Contact() {
+  const [success, setSuccess] = React.useState(false);
+  const name = useForm(false);
+  const email = useForm("email");
+  const message = useForm("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (email.validate() && message.validate()) {
+      const templateParams = {
+        from_name: name.value,
+        email: email.value,
+        message: message.value,
+      };
+
+      emailjs
+        .send(
+          "service_ovc51pi",
+          "template_ra9q7ua",
+          templateParams,
+          "s_YA3z6mo4qOe0thA"
+        )
+        .then(
+          () => {
+            setSuccess(true);
+            name.setValue("");
+            email.setValue("");
+            message.setValue("");
+          },
+          (err: Error) => {
+            console.log("Erro ao enviar o menssagem");
+            console.log(err);
+            name.setValue("");
+            email.setValue("");
+            message.setValue("");
+          }
+        );
+    }
+  }
+
+  React.useEffect(() => {
+    if (name.error || email.error || message.error) setSuccess(false);
+
+    if (success) {
+      setTimeout(() => {
+        setSuccess(false);
+      }, 10000);
+    }
+  }, [success, name.error, email.error, message.error]);
+
+  return (
+    <section>
+      <Cake />
+      <h2>Entre em contato conosco e faça o seu pedido!</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="fields">
+          <Field label="Nome" type="text" name="name" {...name} />
+          <Field label="Email" type="email" name="email" {...email} />
+          <Field
+            label="Menssagem"
+            type="textarea"
+            name="message"
+            {...message}
+          />
+          <button>Enviar</button>
+          {success && <p className="success">Menssagem enviada com sucesso!</p>}
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nav from "../components/Nav/Nav";
 import Hero from "../components/Hero/Hero";
 import Products from "../containers/Products/Products";
+import Form from "../components/Contact/Contact";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <Nav />
       <Hero />
       <Products />
+      <Form />
     </>
   );
 }
