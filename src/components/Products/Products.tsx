0b9a058d89afde86843c0a8016f6584f736b2e46
brits <joseluizbrits@gmail.com>
2023-11-14 22:02:44 -0300

    starting the animation of the Products component

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,11 +1,57 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { Section } from "./ProductsStyled";
 import Button from "../Button/Button";
 import { products } from "../../utils/products";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 function Produtos() {
+  React.useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.from(".section-products .title", {
+        scrollTrigger: {
+          trigger: ".section-products .title",
+          start: "top center",
+          end: "bottom center",
+        },
+        x: -600,
+        opacity: 0,
+      });
+
+      gsap.from(".candy .image", {
+        scrollTrigger: {
+          trigger: ".candy",
+          start: "top 80%",
+          end: "bottom center",
+        },
+        scale: 0,
+      });
+
+      products.forEach(({ type }) => {
+        gsap.from(`.candy.${type}`, {
+          scrollTrigger: {
+            trigger: `.candy.${type}`,
+            toggleActions: "play none none none",
+            start: "top 80%",
+            end: "bottom center",
+            markers: true,
+          },
+          opacity: 0,
+          x: 100,
+          delay: 0.5,
+        });
+      });
+    });
+
+    return () => ctx.revert();
+  });
+
   return (
-    <Section>
+    <Section className="section-products">
       <div className="title">
         <h2>Sabor feito com carinho</h2>
 
@@ -21,7 +67,6 @@ function Produtos() {
           <div key={name} className={`candy ${type}`}>
             <NavLink to={`/${type}`} className="image">
               <img src={img} alt={alt} id={type} />
-              <div className="shadow"></div>
             </NavLink>
 
             <div className="text">
