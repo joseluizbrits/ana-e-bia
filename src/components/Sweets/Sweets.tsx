80efbb6424bf63126d4ed03aaf0e0c34cc48bc71
brits <joseluizbrits@gmail.com>
2023-11-16 19:03:46 -0300

    Sweets animation done

--- src/components/Sweets/Sweets.tsx
+++ src/components/Sweets/Sweets.tsx
@@ -4,10 +4,44 @@ import { SweetsStyled } from "./SweetsStyled";
 import { products } from "../../utils/products";
 import useMedia from "../../hooks/useMedia";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 
 function Sweets() {
+  React.useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.from("#sweets h2", {
+        scrollTrigger: {
+          trigger: "#sweets",
+          start: "top 80%",
+          end: "bottom center",
+        },
+        x: 200,
+        opacity: 0,
+        ease: "power3.out",
+      });
+
+      gsap.from("#sweets .swiper-slide", {
+        scrollTrigger: {
+          trigger: "#sweets .swiper-slide",
+          start: "top 80%",
+          end: "bottom center",
+        },
+        x: -100,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "elastic(1, .8)",
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   const desktop = useMedia("(max-width: 1400px)");
   const mobile = useMedia("(max-width: 900px)");
   const pageName = useLocation().pathname.replace("/", "");
@@ -19,7 +53,10 @@ function Sweets() {
   }, [pageName]);
 
   return (
-    <SweetsStyled className={pageName === "sobre" ? "sweets-about" : ""}>
+    <SweetsStyled
+      id="sweets"
+      className={pageName === "sobre" ? "sweets-about" : ""}
+    >
       <div className="wrapp">
         {pageName === "sobre" ? (
           <h2>Veja os nossos doces</h2>
