f30c11f2676573898491772aa059e98618b0d407
brits <joseluizbrits@gmail.com>
2023-11-16 18:36:10 -0300

    AboutConclusion animation done

--- src/components/AboutConclusion/AboutConclusion.tsx
+++ src/components/AboutConclusion/AboutConclusion.tsx
@@ -1,9 +1,54 @@
+import React from "react";
 import { AboutConclusionStyled } from "./AboutConclusionStyled";
 import Donut from "../../assets/svg/Donut";
 
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 function AboutConclusion() {
+  React.useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.registerPlugin(ScrollTrigger);
+
+      gsap.from(".section-aboutConclusion .text :nth-child(1)", {
+        scrollTrigger: {
+          trigger: ".section-aboutConclusion",
+          start: "20% 80%",
+          end: "75% center",
+          scrub: 1,
+        },
+        x: -200,
+        opacity: 0.6,
+      });
+
+      gsap.from(".section-aboutConclusion .text :nth-child(2)", {
+        scrollTrigger: {
+          trigger: ".section-aboutConclusion",
+          start: "20% 80%",
+          end: "75% center",
+          scrub: 1,
+        },
+        x: 200,
+        opacity: 0.6,
+      });
+
+      gsap.from(".section-aboutConclusion svg", {
+        scrollTrigger: {
+          trigger: ".section-aboutConclusion svg",
+          start: "top center",
+          end: "60% center",
+          scrub: 1,
+        },
+        x: -200,
+        y: 60,
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <AboutConclusionStyled>
+    <AboutConclusionStyled className="section-aboutConclusion">
       <div className="text">
         <p>
           Em cada bolo, cookie, macaron e sobremesa que oferecemos você encontra
