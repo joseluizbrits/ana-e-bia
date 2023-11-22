ccde2d31bb253517d19e75a3fb0ed7af518a9168
brits <joseluizbrits@gmail.com>
2023-11-22 13:14:15 -0300

    Updates

--- assets/Sweets-38ed474c.js
+++ assets/Sweets-38ed474c.js
@@ -1,4 +1,4 @@
-import{s as Be,R as z,g as X,S as Re,r as L,u as me,a as Fe,p as $e,j as N,L as je}from"./index-35ed53b7.js";const ke="/ana-e-bia/assets/arrowCircle-cd5024ab.svg",He=Be.div`
+import{s as Be,R as z,g as X,S as Re,r as L,u as me,a as Fe,p as $e,j as N,L as je}from"./index-a9e9481c.js";const ke="/assets/arrowCircle-cd5024ab.svg",He=Be.div`
   overflow: hidden;
 
   .wrapp {
